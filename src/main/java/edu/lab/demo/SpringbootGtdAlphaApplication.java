604f86b1d10e9cb8e777b0291116d4e94a8c5e48
cpccharry <harry.subramaniam@cpcc.edu>
2020-11-23 19:14:57 -0500

    Initial release.

---
+++ src/main/java/edu/lab/demo/SpringbootGtdAlphaApplication.java
@@ -0,0 +1,13 @@
+package edu.lab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGtdAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGtdAlphaApplication.class, args);
+	}
+
+}
